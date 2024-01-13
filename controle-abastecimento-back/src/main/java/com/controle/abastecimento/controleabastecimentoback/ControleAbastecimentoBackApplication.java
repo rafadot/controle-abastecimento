4b568045d93f81fbb0643c6ae3a296606ad9571d
Rafael <rafaelairesdemedeiros@outlook.com>
2024-01-13 17:23:12 -0300

    first commit

---
+++ controle-abastecimento-back/src/main/java/com/controle/abastecimento/controleabastecimentoback/ControleAbastecimentoBackApplication.java
@@ -0,0 +1,13 @@
+package com.controle.abastecimento.controleabastecimentoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAbastecimentoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAbastecimentoBackApplication.class, args);
+	}
+
+}
---
+++ controle-abastecimento-back/src/test/java/com/controle/abastecimento/controleabastecimentoback/ControleAbastecimentoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controle.abastecimento.controleabastecimentoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAbastecimentoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
